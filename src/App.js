911a47dcc02d5fbf34466d164221406a3b5a5248
Oluwatobiloba Oladunjoye <dunjoyebiz2020@gmail.com>
2024-02-12 16:31:57 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg'
+import './App.css';
+
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <ParticlesBg type="cobweb" color="#ffffff" num={200} bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        
+        {/* 
+          
+          
+          <FaceRecognition /> */}
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Rank = () => {
+    return(
+       <div>
+        <div className="white f1">
+            {'Tobi, your current rank is... #5'}
+
+        </div>
+       </div>
+    );
+}
+
+export default Rank;
